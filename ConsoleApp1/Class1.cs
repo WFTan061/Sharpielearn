05acb383616c220ac6a94889940b1275316aefa0
wf <wuafong92@Live.com.sg>
2020-12-21 17:05:32 +0800

    17-20

--- ConsoleApp1/Class1.cs
+++ ConsoleApp1/Class1.cs
@@ -8,11 +8,90 @@ namespace ConsoleApp1
     {
         static void Main()
         {
-            int x = 5;
-            String y = "5";
-            var result = x + int.Parse(y);
-            Console.WriteLine("Result of x + y is " + result.ToString);
-            Console.ReadLine();
+            Car newCar = new Car();
+            Car otherCar = newCar;
+            newCar.make = "Toyota";
+            newCar.model = "Vios";
+            newCar.year = 2019;
+            newCar.color = "white";
+            Car secondCar = new Car("Nissan", "Sunny", 2009, "black");
+
+            otherCar.writeString();//same as first new car
+            Console.WriteLine("{0} - {1} - {2} - {3}", newCar.make, newCar.model, newCar.year, newCar.color);
+            Console.WriteLine("Car's worth : {0:C}", newCar.determineMarketValue());
+            secondCar.writeString();
+            newCar = null;
+            otherCar.writeString();//Should exception.
+            Console.WriteLine("Break");
+            Truck truck = new Truck();
+            truck.make = "Toyota";
+            truck.model = "Truck";
+            truck.year = 2019;
+            truck.color = "Blue";
+            truck.towingLine = 12500;
+            truck.writeString();
+
+        }
+        public static double determineMarketValue(Car car)
+        {
+            double carValue = 100.0;
+            if (car.year > 1999)
+            {
+                carValue = 10000.0;
+            }
+            else
+            {
+                carValue = 2000.0;
+            }
+            return carValue;
+        }
+
+    }
+
+    abstract class Vehicle{
+        public string make { get; set; }
+        public string model { get; set; }
+        public int year { get; set; }
+        public string color { get; set; }
+        public abstract void writeString();
+    }
+
+    class Car : Vehicle
+    {
+        public double determineMarketValue()
+        {
+            double carValue = 100.0;
+            if (this.year > 1999)
+            {
+                carValue = 10000.0;
+            }
+            else
+            {
+                carValue = 2000.0;
+            }
+            return carValue;
+        }
+        public Car()//similar to java normal
+        {
+            this.make = "Nissan";
+        }
+        public Car(String make, String model, int year, String color) {//similar to java normal
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.color = color;
+        }
+        public override void writeString()
+        {
+            Console.WriteLine("{0} - {1} - {2} - {3}", this.make, this.model, this.year, this.color);
+        }
+    }
+    class Truck : Car //truck inherits car!
+    {
+        public int towingLine { get; set; }
+        public override void writeString()
+        {
+            Console.WriteLine("{0} - {1} - {2} - {3} - {4}", this.make, this.model, this.year, this.color,this.towingLine);
         }
     }
 }
---
+++ ConsoleApp1/EnumerationSwitch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class EnumerationSwitch
+    {
+        static void Main(string[] args) {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            String userValue = Console.ReadLine();
+            LeegrChamps userChamp;
+            if(Enum.TryParse<LeegrChamps>(userValue,true,out userChamp))
+            {
+                switch (userChamp)
+                {
+                    case LeegrChamps.Garen:
+                        Console.WriteLine("Garen!");
+                        break;
+                    case LeegrChamps.Jax:
+                        Console.WriteLine("Jax!");
+                        break;
+                    case LeegrChamps.Orianna:
+                        Console.WriteLine("Orianna!");
+                        break;
+                    default:
+                        Console.WriteLine("BLANK!");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Does Not COmpute!");
+            }
+
+        }
+
+    }
+    enum LeegrChamps
+    {
+        Garen,
+        Jax,
+        Orianna
+    }
+}
